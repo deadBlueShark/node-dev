5dad93355a28f88961e523ce65631b071c650419
chiNguyenTrex <xspacechinguyen@gmail.com>
2019-09-29 23:40:20 +0700

    Retrieve and persist message data to mongodb

--- 0003-chat-app/server.js
+++ 0003-chat-app/server.js
@@ -5,8 +5,8 @@ let app = express();
 let http = require('http').Server(app);
 let io = require('socket.io')(http);
 
-
-let bodyParser = require('body-parser'); // Use to parse request body to JSON
+// Use to parse request body to JSON
+let bodyParser = require('body-parser');
 
 // app.listen(3000);
 http.listen(3000);
@@ -16,22 +16,70 @@ app.use(express.static(__dirname));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-let messages = [
-  { name: 'David', content: 'Hi'},
-  { name : 'James', content: 'Nice to meet you' }
-]
+// MongoDB object modeling for NodeJS
+let mongoose = require('mongoose');
+let dbUrl = 'mongodb://lechinguyen:nguyen123456@ds157864.mlab.com:57864/nguyen-node';
+
+// mongoose.connect(dbUrl, {useNewUrlParser: true, useUnifiedTopology: true}, (err) => {
+//   console.log('MongoDB connection: ', err);
+// })
+// User promise version
+mongoose.connect(dbUrl, {useNewUrlParser: true, useUnifiedTopology: true})
+  .then(() => console.log('DB connected'))
+  .catch(err => console.log('Connect failed: ', err));
+
+// Create Message schema
+let Schema = mongoose.Schema;
+// Design model
+let messageSchema = new Schema({ name: String, content: String });
+// Create model
+let Message = mongoose.model('Message', messageSchema);
+
+// let messages = [
+//   { name: 'David', content: 'Hi'},
+//   { name : 'James', content: 'Nice to meet you' }
+// ]
 
 app.get('/messages', (req, res) => {
-  res.send(messages);
+  Message.find({}).then(messages => {
+    res.send(messages);
+  }).catch(err => {
+    console.log('Retrieve data faild: ', err)
+  });
 })
 
 app.post('/messages', (req, res) => {
   console.log(req.body);
-  messages.push(req.body);
+  // messages.push(req.body);
 
-  // Emit a 'message' event to client
-  io.emit('message', req.body);
-  res.sendStatus(200);
+  let message = new Message(req.body);
+  console.log(message);
+  // Persist message to DB
+  Message.create(message)
+    .then(result => {
+      console.log('Persist data successfully: ', result);
+
+      // Remove bad messages
+      return Message.findOne({content: 'badword'});
+    })
+    .then((data) => {
+      if(data) {
+        console.log('Bad word found: ', data);
+        // Remove bad word
+        return Message.remove({_id: data.id});
+      }
+
+      // Emit a 'message' event to client
+      io.emit('message', req.body);
+      res.sendStatus(200);
+    })
+    .then(() => {
+      console.log('Bad word removed.');
+    })
+    .catch(err => {
+      console.log('Persist data failed: ', err);
+      res.sendStatus(400);
+    });
 })
 
 // Make o 'connection' event to client
