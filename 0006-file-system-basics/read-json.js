d39724adab5173b13c1333ef5aec9e95c6c4e3d2
chiNguyenTrex <xspacechinguyen@gmail.com>
2019-10-02 22:43:11 +0700

    finish node essential

--- 0006-file-system-basics/read-json.js
+++ 0006-file-system-basics/read-json.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
-
 let data = require('./assets/style/info.json');
 
 console.log(data);
 data.colors.forEach(item => {
-  fs.appendFile('./assets/style/mirror-info.json', `${item.name}: ${item.id}\n`, err => {
+  let colorText = `${item.name}: ${item.id}\n`;
+  fs.appendFile('./assets/style/mirror-info.json', colorText, err => {
     if (err) throw err;
     console.log('Written');
   });
