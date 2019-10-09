3ce86f1cb3cb5591427baa0333f2273904349dc6
chiNguyenTrex <xspacechinguyen@gmail.com>
2019-10-09 23:28:42 +0700

    image processing v2

--- image-processing-v1/index.js
+++ image-processing-v1/index.js
@@ -1,26 +1,13 @@
 var async = require('async');
 var AWS = require('aws-sdk');
-var gm = require('gm')
-            .subClass({ imageMagick: true }); // Enable ImageMagick integration.
+var gm = require('gm').subClass({ imageMagick: true });
 var util = require('util');
 var DEFAULT_MAX_WIDTH  = 200;
 var DEFAULT_MAX_HEIGHT = 200;
-var DDB_TABLE = 'images';
+var DDB_TABLE = 'action-images';
 var s3 = new AWS.S3();
 var dynamodb = new AWS.DynamoDB();
-function getImageType(key, callback) {
-  var typeMatch = key.match(/\.([^.]*)$/);
-  if (!typeMatch) {
-      callback("Could not determine the image type for key: ${key}");
-      return;
-  }
-  var imageType = typeMatch[1];
-  if (imageType != "jpg" && imageType != "png") {
-      callback('Unsupported image type: ${imageType}');
-      return;
-  }
-  return imageType;
-}
+
 exports.handler = (event, context, callback) => {
   console.log("Reading options from event:\n",
     util.inspect(event, {depth: 5}));
@@ -31,52 +18,55 @@ exports.handler = (event, context, callback) => {
      var imageType = getImageType(srcKey, callback);
   async.waterfall([
     function downloadImage(next) {
-      s3.getObject({
-          Bucket: srcBucket,
-          Key: srcKey
-        },
-        next);
-      },
+      s3.getObject({ Bucket: srcBucket, Key: srcKey }, next);
+    },
     function tranformImage(response, next) {
       gm(response.Body).size(function(err, size) {
+        if (err) {
+          return next(err);
+        }
+
         var metadata = response.Metadata;
         console.log("Metadata:\n", util.inspect(metadata, {depth: 5}));
+
         var max_width;
         if ('width' in metadata) {
           max_width = metadata.width;
         } else {
           max_width = DEFAULT_MAX_WIDTH;
         }
+
         var max_height;
         if ('height' in metadata) {
           max_height = metadata.height;
         } else {
           max_height = DEFAULT_MAX_HEIGHT;
         }
+
         var scalingFactor = Math.min(
           max_width / size.width,
           max_height / size.height
         );
+
         var width  = scalingFactor * size.width;
         var height = scalingFactor * size.height;
-        this.resize(width, height)
-          .toBuffer(imageType, function(err, buffer) {
-            if (err) {
-              next(err);
-            } else {
-              next(null, response.ContentType, metadata, buffer);
-            }
-          });
+        this.resize(width, height).toBuffer(imageType, function(err, buffer) {
+          if (err) {
+            next(err);
+          } else {
+            next(null, response.ContentType, metadata, buffer);
+          }
+        });
       });
     },
     function uploadThumbnail(contentType, metadata, data, next) {
       // Stream the transformed image to a different S3 bucket.
       s3.putObject({
-          Bucket: dstBucket,
-          Key: dstKey,
-          Body: data,
-          ContentType: contentType,
-          Metadata: metadata
+        Bucket: dstBucket,
+        Key: dstKey,
+        Body: data,
+        ContentType: contentType,
+        Metadata: metadata
       }, function(err, buffer) {
         if (err) {
           next(err);
@@ -118,3 +108,16 @@ exports.handler = (event, context, callback) => {
     }
   );
 };
+
+function getImageType(key, callback) {
+  var typeMatch = key.match(/\.([^.]*)$/);
+  if (!typeMatch) {
+    return callback("Could not determine the image type for key: ${key}");
+  }
+  var imageType = typeMatch[1];
+  if (imageType != "jpg" && imageType != "png") {
+    return callback('Unsupported image type: ${imageType}');
+  }
+
+  return imageType;
+}
